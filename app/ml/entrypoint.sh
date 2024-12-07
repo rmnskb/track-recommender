ab717abbadf1432cc03daebd857912a8ab7c83af
Bogdan Romenskii <rmnsk@seznam.cz>
2024-12-08 00:46:52 +0100

    Restucture the ml folder, update the entrypoint script

--- app/ml/entrypoint.sh
+++ app/ml/entrypoint.sh
@@ -2,7 +2,7 @@ MODEL_FILE="kdt.pkl"
 
 if [ ! -f "$MODEL_FILE" ]; then
 	echo "Model file does not exist, training a new one"
-	python recommender.py
+	cd .. && python recommender.py
 else
 	echo "Model file exists. Skipping the training"
 fi
