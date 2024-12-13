cda82f1dc395941d5196d64dad588491ac5a1fa8
Bogdan Romenskii <rmnsk@seznam.cz>
2024-12-13 16:06:04 +0100

    Add Recommendations to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,9 @@ import axios from 'axios'
 const apiUrl = '//localhost:5000'
 
 
-const SearchBar = () => {
+const SearchBar = ({ chosenTracks, setChosenTracks }) => {
     const [query, setQuery] = useState('')
     const [autocomplete, setAutocomplete] = useState([{}])
-    const [chosenTracks, setChosenTracks] = useState([])
 
     const inputHandler = (e) => {
         setQuery(e.target.value);
@@ -18,7 +17,6 @@ const SearchBar = () => {
     const fetchAutocomplete = async (query) => {
         try {
             const response = await axios.get(`${apiUrl}/api/v1/autocomplete?q=${query}`);
-            console.log('Response structure: ', response.data);
             setAutocomplete(response.data);
         } catch (error) {
             console.error('Error fetching the autocomplete: ', error);
@@ -43,8 +41,7 @@ const SearchBar = () => {
             <li
                 key={track['track_id']}
                 onClick={() => {
-                    setChosenTracks(track['track_id']);
-                    console.log(track['track_id']);
+                    setChosenTracks([...chosenTracks, track['track_id']]);
                 }}
                 className="active"
             >
@@ -68,8 +65,59 @@ const SearchBar = () => {
     );
 }
 
+
+const Recommendations = (tracks) => {
+    const [recs, setRecs] = useState([{}])
+
+    useEffect(() => {
+            const fetchRecommendations = async () => {
+                if (!tracks || tracks.tracks.length === 0) {
+                    return;
+                }
+
+                const payload = {
+                    'ids': Array.isArray(tracks.tracks) ? tracks.tracks : []
+                    , 'n_recs': 5
+                };
+
+                try {
+                    const response = await axios.post(
+                        `${apiUrl}/api/v1/recommend`
+                        , payload
+                    );
+                    setRecs(response.data)
+                } catch (error) {
+                    console.error('Error fetching recommendations:', error);
+                }
+            };
+            fetchRecommendations();
+        }, [tracks]
+    );
+
+    const listRecommendations = recs.length > 0 ? (
+        recs.map(rec =>
+            <li
+                key={rec['track_id']}
+            >
+                {rec['track_artist']}
+            </li>
+        )
+    ) : (
+        <li>No recommendations</li>
+    );
+
+    return (
+      <div>
+          <li>
+              {recs && listRecommendations}
+          </li>
+      </div>
+    );
+}
+
 export default function App() {
     const [test, setTest] = useState('')
+    const [chosenTracks, setChosenTracks] = useState([])
 
     const testApi = async () => {
         try {
@@ -88,7 +136,10 @@ export default function App() {
                 <a>{test['api_status']}</a>
             </div>
             <div>
-                <SearchBar />
+                <SearchBar chosenTracks={chosenTracks} setChosenTracks={setChosenTracks} />
+            </div>
+            <div>
+                <Recommendations tracks={chosenTracks} />
             </div>
         </div>
     );
