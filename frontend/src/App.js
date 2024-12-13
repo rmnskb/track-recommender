7d1c0fc1e4893673d9e3f37bfd8d4aec464e3b43
Bogdan Romenskii <rmnsk@seznam.cz>
2024-12-13 14:17:19 +0100

    Implement basic search bar with autocomplete functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios'
 
+const apiUrl = '//localhost:5000'
+
+
+const SearchBar = () => {
+    const [query, setQuery] = useState('')
+    const [autocomplete, setAutocomplete] = useState([{}])
+    const [chosenTracks, setChosenTracks] = useState([])
+
+    const inputHandler = (e) => {
+        setQuery(e.target.value);
+    };
+
+    const fetchAutocomplete = async (query) => {
+        try {
+            const response = await axios.get(`${apiUrl}/api/v1/autocomplete?q=${query}`);
+            console.log('Response structure: ', response.data);
+            setAutocomplete(response.data);
+        } catch (error) {
+            console.error('Error fetching the autocomplete: ', error);
+            setAutocomplete([]);
+        }
+    }
+
+    useEffect(() => {
+        if (query.length > 2) {
+            const timeoutId = setTimeout(() => {
+                fetchAutocomplete(query);
+            }, 300); // Adjust debounce delay as needed (300ms is typical)
+
+            return () => clearTimeout(timeoutId); // Cleanup timeout on query change
+        } else {
+            setAutocomplete([]);
+        }
+    }, [query]);
+
+    const listAutocomplete = autocomplete.length > 0 ? (
+        autocomplete.map(track =>
+            <li
+                key={track['track_id']}
+                onClick={() => {
+                    setChosenTracks(track['track_id']);
+                    console.log(track['track_id']);
+                }}
+                className="active"
+            >
+                {track['track_artist']}
+            </li>
+        )
+    ) : (
+        <li>No suggestions available</li>
+    );
+
+    return (
+        <div>
+            <input
+            type="text"
+            placeholder="Enter your search here"
+            onChange={inputHandler}
+            value={query}
+        />
+        <ul>{autocomplete && listAutocomplete}</ul>
+        </div>
+    );
+}
+
 export default function App() {
-    const apiUrl = 'http://localhost:5000'
     const [test, setTest] = useState('')
+
     const testApi = async () => {
         try {
             const response = await axios.get(`${apiUrl}/api/v1/test`);
@@ -22,6 +87,9 @@ export default function App() {
             <div>
                 <a>{test['api_status']}</a>
             </div>
+            <div>
+                <SearchBar />
+            </div>
         </div>
-    )
+    );
 }
