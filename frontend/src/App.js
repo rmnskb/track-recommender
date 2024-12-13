bd97f6122dc830b3fe526145b30ff763f3479ea0
Bogdan Romenskii <rmnsk@seznam.cz>
2024-12-13 02:02:52 +0100

    Connect React with Flask

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const apiUrl = 'http://localhost:5000'
+    const [test, setTest] = useState('')
+    const testApi = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/api/v1/test`);
+            setTest(response.data)
+        } catch (error) {
+            console.error('Error testing the API: ', error)
+        }
+    }
+
+    return (
+        <div>
+            <h1>Music Recommender</h1>
+            <button onClick={testApi}>Test the API</button>
+            <div>
+                <a>{test['api_status']}</a>
+            </div>
+        </div>
+    )
 }
-
-export default App;
