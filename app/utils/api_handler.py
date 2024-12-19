cfd99540c77dfa2c7565cb458759451218510ed2
Bogdan Romenskii <rmnsk@seznam.cz>
2024-12-19 19:10:00 +0100

    Extend the API with extra functionality

--- app/utils/api_handler.py
+++ app/utils/api_handler.py
@@ -1,4 +1,19 @@
 import requests
+from functools import wraps
+
+
+def wait_on_429(func):
+    @wraps(func)
+    def wrapper_wait(self, *args, **kwargs):
+        result = func(self, *args, **kwargs)
+
+        if isinstance(result, tuple) and len(result) >= 2 and result[1] == 429:
+            asyncio.sleep(10)
+            result = func(self, *args, **kwargs)
+
+        return result
+
+    return wrapper_wait
 
 
 class APIHandler:
@@ -7,7 +22,7 @@ class APIHandler:
             self._client_id = client_id
             self._client_secret = client_secret
 
-    def post(self, url, headers, payload, use_secret: bool = False, **kwargs) -> dict:
+    def post(self, url, headers, payload, use_secret: bool = False, **kwargs) -> tuple[dict, int]:
         if use_secret and self._client_id and self._client_secret:
             payload['client_id'] = self._client_id
             payload['client_secret'] = self._client_secret
@@ -17,33 +32,40 @@ class APIHandler:
 
         try:
             response = requests.post(url=url, headers=headers, data=payload)
-            response.raise_for_status()
 
             try:
-                return response.json()
+                return response.json(), response.status_code
             except requests.exceptions.JSONDecodeError as err:
-                print(err)
                 return {
                     'response': response.text
-                }
-        except requests.exceptions.HTTPError as err:
-            print(err)
-            return {}
+                }, response.status_code
+        except requests.exceptions.ConnectionError as err:
+            return {}, 503
 
     @staticmethod
-    def get(url, **kwargs) -> dict:
+    def get(url, **kwargs) -> tuple[dict, int]:
         try:
-            response = requests.get(url, **kwargs)
-            response.raise_for_status()
+            async with requests.Session() as session:
+                request = requests.Request('GET', url=url, **kwargs)
+                prepped = session.prepare_request(request)
+
+                if prepped.headers:
+                    # prepped.headers = prepped.headers
+                    print(prepped.headers)
+
+                response = session.send(prepped)
 
             try:
-                return response.json()
+                return response.json(), response.status_code
             except requests.exceptions.JSONDecodeError as err:
-                print(err)
                 return {
                     'response': response.text
-                }
-        except requests.exceptions.HTTPError as err:
-            print(err)
-            return {}
+                }, response.status_code
+        except requests.exceptions.ConnectionError as err:
+            return {}, 503
+
+
+if __name__ == '__main__':
+    api = APIHandler()
+    api.get(url='http://127.0.0.1:5000/api/v1/autocomplete?q=Come', headers={'Authorization': 'Bearer  123'})
 
--- app/utils/spotify_api.py
+++ app/utils/spotify_api.py
@@ -1,9 +1,28 @@
 import os
 import requests
 from dotenv import load_dotenv
-from api_handler import APIHandler
+from functools import wraps
+from api_handler import APIHandler, wait_on_429
 
 load_dotenv()
+
+
+def reauthorize_on_401(func):
+    @wraps(func)
+    def wrapper_reauthorize(self, *args, **kwargs):
+        if not self._access_token:
+            self._authenticate()
+
+        result = func(self, *args, **kwargs)
+
+        if isinstance(result, tuple) and len(result) >= 2 and result[1] == 401:
+            self._authenticate()
+
+            result = func(self, *args, **kwargs)
+
+        return result
+
+    return wrapper_reauthorize
 
 
 class SpotifyAPIHandler(APIHandler):
@@ -16,32 +35,57 @@ class SpotifyAPIHandler(APIHandler):
         self._access_token = ''
         self._token_type = ''
 
-    def _authenticate(self):
-        try:
-            response = self.post(
-                url='https://accounts.spotify.com/api/token'
-                , headers={
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-                , payload={
-                    'grant_type': 'client_credentials'
-                }
-                , use_secret=True
-            )
+    def _authenticate(self) -> None:
+        response, status = self.post(
+            url='https://accounts.spotify.com/api/token'
+            , headers={
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            , payload={
+                'grant_type': 'client_credentials'
+            }
+            , use_secret=True
+        )
 
-            self._access_token = response['access_token']
-            self._token_type = response['token_type']
+        if status == 200:
+            self._access_token = response['access_token'].replace(' ', '')
+            self._token_type = response['token_type'].replace(' ', '')
 
-            print(response)
+    @wait_on_429
+    @reauthorize_on_401
+    def get_track(self, track_id: str) -> dict:
+        # if not self._access_token:
+        #     self._authenticate()
 
-        except requests.exceptions.HTTPError as err:
-            print(err)
+        response, status = self.get(
+            url=f'https://api.spotify.com/v1/tracks/{track_id}'
+            , headers={'Authorization': f'{self._token_type}  {self._access_token}'}
+        )
 
-    def get_artists(self, artist_id: str) -> dict:
-        ...
+        if status == 200:
+            return response
+        # elif status == 401:
+        #     self._authenticate()
+        #     self.get_track(track_id=track_id)
+
+        return response
+
+    @wait_on_429
+    @reauthorize_on_401
+    def get_tracks(self, track_ids: list[str]) -> dict:
+        ids = ','.join(track_ids)
+
+        response, status = self.get(
+            url=f'https://api.spotify.com/v1/tracks?ids={ids}'
+            , headers={'Authorization': f'{self._token_type}  {self._access_token}'}
+        )
+
+        if status == 200:
+            return response
 
 
 if __name__ == '__main__':
     api = SpotifyAPIHandler()
 
-    print(api._authenticate())
+    # print(api.get_track(track_id='5SuOikwiRyPMVoIQDJUgSV'))
+    # print(api.get_tracks(track_ids=['7ouMYWpwJ422jRcDASZB7P', '4VqPOruhp5EdPBeR92t6lQ', '2takcwOaAZWiXQijPHIx7B']))
